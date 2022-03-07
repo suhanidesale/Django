b63fc721c3e7be5354c9202d90cf9fec83b092de
suhanidesale <deslesuhani02@gmail.com>
2022-03-07 21:41:14 +0530

    django 3

--- django_eportfolio/home/views.py
+++ django_eportfolio/home/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from matplotlib.style import context
 
 # Create your views here.
 def home(request):
     # return HttpResponse("This is the home page")
-    return render(request , 'home.html')
+    context = {'name': 'suhani', 'course': 'information technology'}
+    return render(request , 'home.html' , context)
 
 def about(request):
     return HttpResponse("This is the about")
