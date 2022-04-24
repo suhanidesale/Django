93cc36b9fd8af645205214970e323550d73f34a4
suhanidesale <deslesuhani02@gmail.com>
2022-04-24 15:52:57 +0530

    contact : db connected

--- django_eportfolio/home/views.py
+++ django_eportfolio/home/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from matplotlib.style import context
-
+from  home.models import Contact
 # Create your views here.
 def home(request):
     # return HttpResponse("This is the home page")
@@ -21,4 +21,7 @@ def contact(request):
         email = request.POST['email']
         desc = request.POST['desc']
         print(name,email,desc)
+        ins = Contact(name=name , email = email , desc = desc)
+        ins.save()
+        print("data has been saved to db")
     return HttpResponse("This is the contact")
